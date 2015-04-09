6ccc7090cbe0f9d088357253decff2d5bdecd585
Hirokazu Miyaji <hirokazu.miyaji@gmail.com>
2015-04-09 11:02:24 +0900

    add mysql

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM python:2.7
 ADD app /app
 WORKDIR /app
-RUN pip install django redis
+RUN pip install django redis mysqlclient
 CMD cd /app && python manage.py test
--- app/app/settings.py
+++ app/app/settings.py
@@ -58,9 +58,17 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #},
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'default',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'mysql',
+        'PORT': '',
     }
 }
 
